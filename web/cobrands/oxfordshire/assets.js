a948dc41c431b21898f7e1c161e72354a34e16e5
Struan Donald <struan@exo.org.uk>
2020-06-15 12:17:40 +0100

    [Oxfordshire] prevent reports on non OCC assets
    also improve contrast between assets and labels

--- web/cobrands/oxfordshire/assets.js
+++ web/cobrands/oxfordshire/assets.js
@@ -21,6 +21,70 @@ var defaults = {
     strategy_class: OpenLayers.Strategy.FixMyStreet
 };
 
+var occ_ownernames = [
+    "LocalAuthority", "CountyCouncil"
+];
+
+function occ_owns_feature(f) {
+    return f &&
+           f.attributes &&
+           f.attributes.maintained_by &&
+           OpenLayers.Util.indexOf(occ_ownernames, f.attributes.maintained_by) > -1;
+}
+
+function occ_does_not_own_feature(f) {
+    return !occ_owns_feature(f);
+}
+
+var asset_default_style = new OpenLayers.Style({
+    fillColor: "#868686",
+    fillOpacity: 0.6,
+    strokeColor: "#000000",
+    strokeOpacity: 0.6,
+    strokeWidth: 2,
+    pointRadius: 4,
+    title: 'Not maintained by Oxfordshire County Council. Maintained by ${maintained_by}.'
+});
+
+var occ_default = new OpenLayers.Style({
+    fillColor: "#007258",
+    fillOpacity: 0.6,
+    strokeColor: "#000000",
+    strokeOpacity: 0.8,
+    strokeWidth: 2,
+    pointRadius: 6
+});
+
+var rule_owned = new OpenLayers.Rule({
+    filter: new OpenLayers.Filter.FeatureId({
+        type: OpenLayers.Filter.Function,
+        evaluate: occ_owns_feature
+    }),
+    symbolizer: {
+        fillColor: "#007258",
+        pointRadius: 6,
+        title: '',
+    }
+});
+
+var rule_not_owned = new OpenLayers.Rule({
+    filter: new OpenLayers.Filter.FeatureId({
+        type: OpenLayers.Filter.Function,
+        evaluate: occ_does_not_own_feature
+    })
+});
+
+asset_default_style.addRules([rule_owned, rule_not_owned]);
+
+var asset_stylemap = new OpenLayers.StyleMap({
+    'default': asset_default_style,
+    'select': fixmystreet.assets.style_default_select,
+    'hover': new OpenLayers.Style({
+        pointRadius: 8,
+        cursor: 'pointer'
+    })
+});
+
 fixmystreet.assets.add(defaults, {
     wfs_feature: "Trees",
     asset_id_field: 'Ref',
@@ -46,13 +110,14 @@ var streetlight_select = $.extend({
     fontColor: "#FFD800",
     labelOutlineColor: "black",
     labelOutlineWidth: 3,
-    labelYOffset: 65,
-    fontSize: '15px',
-    fontWeight: 'bold'
+    labelYOffset: 69,
+    fontSize: '18px',
+    fontWeight: 'bold',
+    fillColor: "#007258"
 }, fixmystreet.assets.style_default_select.defaultStyle);
 
 var streetlight_stylemap = new OpenLayers.StyleMap({
-  'default': fixmystreet.assets.style_default,
+  'default': occ_default,
   'select': new OpenLayers.Style(streetlight_select)
 });
 
@@ -83,6 +148,7 @@ fixmystreet.assets.add(defaults, {
 });
 
 fixmystreet.assets.add(defaults, {
+    select_action: true,
     // have to do this by hand rather than using wfs_* options
     // as the server does not like being POSTed xml with application/xml
     // as the Content-Type which is what using those options results in.
@@ -104,12 +170,25 @@ fixmystreet.assets.add(defaults, {
     attributes: {
         feature_id: 'id'
     },
+    stylemap: asset_stylemap,
     asset_category: ["Gully and Catchpits"],
     asset_item: 'drain',
-    filter_key: 'maintained_by',
-    filter_value: [
-        "LocalAuthority"
-    ]
+    actions: {
+        asset_found: function(asset) {
+          var is_occ = occ_owns_feature(asset);
+          if (!is_occ) {
+              $("js-not-local-authority-asset").removeClass('hidden');
+              fixmystreet.message_controller.asset_not_found.call(this);
+          } else {
+              $("js-not-local-authority-asset").addClass('hidden');
+              $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+          }
+        },
+        asset_not_found: function() {
+            $("js-not-local-authority-asset").addClass('hidden');
+           $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+        }
+    }
 });
 
 fixmystreet.assets.add(defaults, {
@@ -134,12 +213,9 @@ fixmystreet.assets.add(defaults, {
     attributes: {
         feature_id: 'id'
     },
+    stylemap: asset_stylemap,
     asset_category: ["Ice/Snow"],
-    asset_item: 'grit bin',
-    filter_key: 'maintained_by',
-    filter_value: [
-        "LocalAuthority", "CountyCouncil"
-    ]
+    asset_item: 'grit bin'
 });
 
 var highways_style = new OpenLayers.Style({
